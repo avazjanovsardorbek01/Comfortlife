c1673404cd57b72e6bc89625230cfd80c09cd41c
avazjanovsardorbek01 <sardoravazzanov@gmail.com>
2024-12-02 15:58:32 +0500

    Update

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -7,7 +7,8 @@ const Footer = () => {
     <div className="site-footer">
       <div className="container">
         <div className="row">
-          <div className="col-lg-4">
+          {/* Contact Section */}
+          <div className="col-12 col-sm-6 col-md-4 col-lg-4 mb-4 mb-lg-0">
             <div className="widget">
               <h3>{t("contact")}</h3>
               <address>{t("location_value")}</address>
@@ -26,56 +27,60 @@ const Footer = () => {
             </div>
           </div>
 
-          <div className="col-lg-4">
+          {/* Sources Section */}
+          <div className="col-12 col-sm-6 col-md-4 col-lg-4 mb-4 mb-lg-0">
             <div className="widget">
               <h3>{t("sources")}</h3>
-              <ul className="list-unstyled float-start links">
-                <li>
-                  <a href="#">About us</a>
-                </li>
-                <li>
-                  <a href="#">Services</a>
-                </li>
-                <li>
-                  <a href="#">Vision</a>
-                </li>
-                <li>
-                  <a href="#">Mission</a>
-                </li>
-                <li>
-                  <a href="#">Terms</a>
-                </li>
-                <li>
-                  <a href="#">Privacy</a>
-                </li>
-              </ul>
-              <ul className="list-unstyled float-start links">
-                <li>
-                  <a href="#">Partners</a>
-                </li>
-                <li>
-                  <a href="#">Business</a>
-                </li>
-                <li>
-                  <a href="#">Careers</a>
-                </li>
-                <li>
-                  <a href="#">Blog</a>
-                </li>
-                <li>
-                  <a href="#">FAQ</a>
-                </li>
-                <li>
-                  <a href="#">Creative</a>
-                </li>
-              </ul>
+              <div className="d-flex flex-wrap justify-content-between">
+                <ul className="list-unstyled links">
+                  <li>
+                    <a href="#">About us</a>
+                  </li>
+                  <li>
+                    <a href="#">Services</a>
+                  </li>
+                  <li>
+                    <a href="#">Vision</a>
+                  </li>
+                  <li>
+                    <a href="#">Mission</a>
+                  </li>
+                  <li>
+                    <a href="#">Terms</a>
+                  </li>
+                  <li>
+                    <a href="#">Privacy</a>
+                  </li>
+                </ul>
+                <ul className="list-unstyled links">
+                  <li>
+                    <a href="#">Partners</a>
+                  </li>
+                  <li>
+                    <a href="#">Business</a>
+                  </li>
+                  <li>
+                    <a href="#">Careers</a>
+                  </li>
+                  <li>
+                    <a href="#">Blog</a>
+                  </li>
+                  <li>
+                    <a href="#">FAQ</a>
+                  </li>
+                  <li>
+                    <a href="#">Creative</a>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
 
-          <div className="col-lg-4">
+          {/* Links Section */}
+          <div className="col-12 col-sm-6 col-md-4 col-lg-4 mb-4 mb-lg-0">
             <div className="widget">
               <h3>{t("links")}</h3>
-              <ul className="list-unstyled links">
+              <ul className="list-unstyled links ">
                 <li>
                   <Link to="/properties">{t("properties")}</Link>
                 </li>
@@ -87,7 +92,8 @@ const Footer = () => {
                 </li>
               </ul>
 
-              <ul className="list-unstyled social">
+              {/* Social Links */}
+              <ul className="list-unstyled social d-flex flex-wrap justify-content-start">
                 <li className="list-inline-item">
                   <a href={import.meta.env.VITE_YOU_TUBE_URL} target="_blank">
                     <span className="icon-youtube"></span>
@@ -126,6 +132,7 @@ const Footer = () => {
           </div>
         </div>
 
+        {/* Footer Bottom */}
         <div className="row mt-5">
           <div className="col-12 text-center">
             <p>
